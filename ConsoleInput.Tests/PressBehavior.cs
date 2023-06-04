407ab84e3aa96236ece95901ade8455397fdfb31
Kresten Laust <krestenlaust@gmail.com>
2023-06-04 18:35:24 +0200

    Added empty test class with methods.

---
+++ ConsoleInput.Tests/PressBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleInput.Tests
+{
+    /// <summary>
+    /// Tests the behavior of the press state.
+    /// </summary>
+    [TestClass]
+    public class PressBehavior
+    {
+        /// <summary>
+        /// We intend for the press state to turn itself off, after a single cycle of being enabled.
+        /// </summary>
+        [TestMethod]
+        public void TestPressTurnsOff()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        /// <summary>
+        /// We intend for the press state to turn itself on, after the last cycle of 'down' state.
+        /// </summary>
+        [TestMethod]
+        public void TestPressTurnsOn()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ ConsoleInput.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleInput.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleInput.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bb250eb-5aa5-48d2-addf-13874e779fe2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
