e315740fed89c2ba0b5793666ffe87f4ebc3e47b
Kresten Laust <krestenlaust@gmail.com>
2023-07-15 17:51:31 +0200

    Moved calculation into static methods

--- ConsoleInput.Logic/DataFlipFlop.cs
+++ ConsoleInput.Logic/DataFlipFlop.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// Outputs the rising- and falling edge of a binary signal, based on a separate clock signal.
     /// </summary>
+    /// TODO: Make SoA equivelent of this class.
     public class DataFlipFlop
     {
         public bool Signal { get; private set; }
@@ -13,10 +14,14 @@
 
         public void ClockSignal(bool newSignal)
         {
-            RisingEdge = newSignal != Signal && newSignal;
-            FallingEdge = newSignal != Signal && !newSignal;
+            RisingEdge = IsRisingEdge(Signal, newSignal);
+            FallingEdge = IsFallingEdge(Signal, newSignal);
 
             Signal = newSignal;
         }
+
+        public static bool IsRisingEdge(bool previousSignal, bool newSignal) => newSignal != previousSignal && newSignal;
+        
+        public static bool IsFallingEdge(bool previousSignal, bool newSignal) => newSignal != previousSignal && !newSignal;
     }
 }
