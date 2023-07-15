47c59fdda63ec95f05c10d501c485cb884d9060a
Kresten Laust <krestenlaust@gmail.com>
2023-07-15 20:20:46 +0200

    Laid groundwork for new input API

---
+++ ConsoleInput/Devices/KeyboardV2.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleInput.Devices
+{
+    // TODO: Make copy of enum to fit name.
+    public class KeyboardV2 : IButtonDevice<Keyboard.VirtualKey>
+    {
+        public bool IsButtonDown(Keyboard.VirtualKey button)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsButtonPressed(Keyboard.VirtualKey button)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsButtonReleased(Keyboard.VirtualKey button)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleInput/Devices/MouseV2.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleInput.Devices
+{
+    public class MouseV2 : IButtonDevice<MouseButton>
+    {
+        public bool IsButtonDown(MouseButton button)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsButtonPressed(MouseButton button)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsButtonReleased(MouseButton button)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
