54e6f9b4da10c5b70fd8721c3af2782704108e9e
Kresten Laust <krestenlaust@gmail.com>
2023-07-15 18:21:55 +0200

    Added dataflipflop helper array

---
+++ ConsoleInput.Logic/DataFlipFlopArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleInput.Logic
+{
+    public class DataFlipFlopArray
+    {
+        public bool[] Signals { get; init set; }
+        readonly bool[] previousSignals;
+
+        public DataFlipFlopArray(int size)
+        {
+            Signals = new bool[size];
+            previousSignals = new bool[size];
+        }
+
+        /// <summary>
+        /// Makes an internal copy of all values of <see cref="Signals"/>, and overrides its previous values.
+        /// NOTE: Doesn't clear <see cref="Signals"/>, only copies its values.
+        /// </summary>
+        public void ClockSignal()
+        {
+            Array.Copy(Signals, previousSignals, Signals.Length);
+        }
+
+        public bool IsRisingEdge(int index) => DataFlipFlop.IsRisingEdge(previousSignals[index], Signals[index]);
+
+        public bool IsFallingEdge(int index) => DataFlipFlop.IsFallingEdge(previousSignals[index], Signals[index]);
+    }
+}
