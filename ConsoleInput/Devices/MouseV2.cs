f26712405f53aa98a37d14edc6023a36878f4699
Kresten Laust <krestenlaust@gmail.com>
2023-07-15 21:59:58 +0200

    Added ICursorDevice

--- ConsoleInput/Devices/MouseV2.cs
+++ ConsoleInput/Devices/MouseV2.cs
@@ -1,7 +1,29 @@
-﻿namespace ConsoleInput.Devices
+﻿using ConsoleInput.Logic;
+
+namespace ConsoleInput.Devices
 {
-    public class MouseV2 : IButtonDevice<MouseButton>
+    public class MouseV2 : IButtonDevice<MouseButton>, ICursorDevice
     {
+        /// <summary>
+        /// The amount of buttons available.
+        /// </summary>
+        public const int MouseButtonCount = 5;
+
+        public MouseV2()
+        {
+            DataFlipFlopArray dff = new DataFlipFlopArray(MouseButtonCount);
+        }
+
+        public short GetX()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public short GetY()
+        {
+            throw new System.NotImplementedException();
+        }
+
         public bool IsButtonDown(MouseButton button)
         {
             throw new System.NotImplementedException();
---
+++ ConsoleInput/ICursorDevice.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleInput
+{
+    // TODO: Consider renaming this.
+    public interface ICursorDevice
+    {
+        /// <summary>
+        /// Gets the column position of the cursor (based on console window).
+        /// </summary>
+        /// <returns></returns>
+        short GetX();
+
+        /// <summary>
+        /// Gets the row position of mouse (based on console window).
+        /// </summary>
+        short GetY();
+    }
+}
