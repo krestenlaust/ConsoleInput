4ae2ee7eec7f58d1ccd4e726c41190ec8ed08985
Kresten Laust <krestenlaust@gmail.com>
2023-07-16 13:26:25 +0200

    Finished KeyboardV2

--- ConsoleInput/Devices/KeyboardV2.cs
+++ ConsoleInput/Devices/KeyboardV2.cs
@@ -1,25 +1,45 @@
-﻿namespace ConsoleInput.Devices
+﻿using ConsoleInput.Logic;
+using ConsoleInput.WinAPI;
+using System;
+
+namespace ConsoleInput.Devices
 {
     public class KeyboardV2 : IDevice, IButtonDevice<KeyboardButton>
     {
-        public bool IsButtonDown(KeyboardButton button)
+        DataFlipFlopArray Dff;
+
+        public KeyboardV2()
         {
-            throw new System.NotImplementedException();
+            Dff = new DataFlipFlopArray(Enum.GetNames(typeof(KeyboardButton)).Length);
         }
 
-        public bool IsButtonPressed(KeyboardButton button)
-        {
-            throw new System.NotImplementedException();
-        }
+        public bool IsButtonDown(KeyboardButton button) => Dff.Signals[GetIndex(button)];
 
-        public bool IsButtonReleased(KeyboardButton button)
-        {
-            throw new System.NotImplementedException();
-        }
+        public bool IsButtonPressed(KeyboardButton button) => Dff.IsRisingEdge(GetIndex(button));
+
+        public bool IsButtonReleased(KeyboardButton button) => Dff.IsFallingEdge(GetIndex(button));
 
         public void Update()
         {
-            throw new System.NotImplementedException();
+            Dff.ClockSignal();
         }
+
+        int GetIndex(KeyboardButton button) => GetIndexFromEnum(button.GetType(), button);
+
+        KeyboardButton GetButton(int index) => (KeyboardButton)GetEnumFromIndex(typeof(KeyboardButton), index);
+
+        /// <summary>
+        /// Called internally by <c>Input</c> when new keyboard events are registered. Updates keyboard state accordingly.
+        /// </summary>
+        /// <param name="keyEvent">The key event retrived by ReadConsoleInput.</param>
+        internal void HandleKeyboardEvent(KEY_EVENT_RECORD keyEvent)
+        {
+            Dff.Signals[GetIndex((KeyboardButton)keyEvent.wVirtualKeyCode)] = keyEvent.bKeyDown;
+        }
+
+        static int GetIndexFromEnum(Type enumType, object enumValue) => Array.IndexOf(Enum.GetValues(enumType), enumValue);
+
+        static object GetEnumFromIndex(Type enumType, int index) => Enum.GetValues(enumType).GetValue(index);
+
     }
 }
