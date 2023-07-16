e2c1e52d8703ce4c1600f7dd741ffcd542f705a2
Kresten Laust <krestenlaust@gmail.com>
2023-07-16 20:15:22 +0200

    Added IDevice dependency on IInputRecordObserver and IRequireConsoleMode#

--- ConsoleInput/IInputRecordObserver.cs
+++ ConsoleInput/IInputRecordObserver.cs
@@ -5,7 +5,7 @@
     /// <see cref="WinAPI.FOCUS_EVENT_RECORD"/>, <see cref="WinAPI.KEY_EVENT_RECORD"/>, <see cref="WinAPI.MOUSE_EVENT_RECORD"/>, <see cref="WinAPI.MENU_EVENT_RECORD"/> or <see cref="WinAPI.WINDOW_BUFFER_SIZE_RECORD"/>.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IInputRecordObserver<T>
+    public interface IInputRecordObserver<T> : IDevice
         where T : WinAPI.IInputRecord
     {
         /// <summary>
--- ConsoleInput/IRequireConsoleMode.cs
+++ ConsoleInput/IRequireConsoleMode.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleInput
+﻿namespace ConsoleInput
 {
-    public interface IRequireConsoleMode
+    public interface IRequireConsoleMode : IDevice
     {
         uint GetConsoleMode();
     }
