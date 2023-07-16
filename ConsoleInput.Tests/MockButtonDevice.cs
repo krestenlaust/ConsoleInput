b617ee532eca705c74073402b3db391a31943fdb
Kresten Laust <krestenlaust@gmail.com>
2023-07-16 22:11:41 +0200

    Added some mock devices for testing

---
+++ ConsoleInput.Tests/MockButtonDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput.Tests
+{
+    internal class MockButtonDevice : IDevice, IButtonDevice<MouseButton>
+    {
+        public bool IsButtonDown(MouseButton button) => Dff.Signals[(int)button];
+
+        public bool IsButtonPressed(MouseButton button) => Dff.IsRisingEdge((int)button);
+
+        public bool IsButtonReleased(MouseButton button) => Dff.IsFallingEdge((int)button);
+
+        public void Update()
+        {
+
+        }
+    }
+}
